c6cafeaac5b01e59894a4a97bb4516554b85e227
scottjohnstone <66835704+scottjohnstone@users.noreply.github.com>
2022-03-30 00:02:53 -0400

    Reworked significantly: 1) changed sleep to not await 2) queries as not async 3) started tasks invoking methods and multicast delegate 4) fleshed out properties and removed status methods

--- FunWithEventsConsole/Program.cs
+++ FunWithEventsConsole/Program.cs
@@ -2,6 +2,9 @@
 
 
 // See https://aka.ms/new-console-template for more information
+//
+// Goal: track the status of long-running asynchronous tasks and inform the user of the overall progress
+//
 
 Console.WriteLine("starting heavy lifting...");
 Heavy.Lifting();
@@ -21,90 +24,88 @@ static public class Heavy
         delegate2 = queries.query2;
         multicastdelegate = delegate1 + delegate2;
 
-        Console.WriteLine("starting delegated processes...");
-        Task t = new Task(delegate { multicastdelegate(); });
-        t.Start();
-        Console.WriteLine(Task.WhenAll(t).Status);
+        // create 3 tasks 
+        Task t1 = new Task(queries.query1);
+        Task t2 = new Task(queries.query2);
+        Task t3 = new Task(new Action(multicastdelegate));
 
+        // start the two methods independent of the delegate
+        t1.Start();
+        t2.Start();
 
-        //        while (multicastdelegate.GetInvocationList().Count() > 0) // just stuff some property into the class and mark it when it starts, in progress, and done - essentially come up with your own task tracking process until you figure out how to do it right
-        while (!queries.QueriesComplete()) // just stuff some property into the class and mark it when it starts, in progress, and done - essentially come up with your own task tracking process until you figure out how to do it right
+        while (!t1.IsCompleted || !t2.IsCompleted)
         {
-            foreach (var item in multicastdelegate.GetInvocationList())
-            {
-                // need to figure out how to access properties within a delegate
-                // in the meantime, workaround by just accessing the two properties on the class fileHandler
-                status = "query1 timer:" + queries.ElapsedTime1() + "query2 timer:" + queries.ElapsedTime2();
-                Console.WriteLine(status + " invo length:" + multicastdelegate.GetInvocationList().Length.ToString() + "done?" + Task.WhenAll(t).IsCompletedSuccessfully.ToString() + "stat:" + Task.WhenAll(t).Status.ToString());
-            }
+            status = "query1 timer:" + queries.ElapsedTimer1 + "query2 timer:" + queries.ElapsedTimer2;
+            Console.WriteLine(status + " task t1:" + t1.Status.ToString() + " task t2:" + t2.Status.ToString());
         }
-        Console.WriteLine("done");
+        Console.WriteLine("done" + " task t1:" + t1.Status.ToString() + " task t2:" + t2.Status.ToString());
+        Thread.Sleep(1000);
+
+        // start the method for the delegate (will run syncrhonously as multicast delegates do)
+        t3.Start();
+        while (!t3.IsCompleted)
+        {
+            status = "query1 timer:" + queries.ElapsedTimer1.ToString() + " query2 timer:" + queries.ElapsedTimer2.ToString();
+            Console.WriteLine(status + " task t3:" + t3.Status.ToString());
+        }
+        Console.WriteLine("done" + " task t3:" + t3.Status.ToString());
     }
 }
 
 public class Queries
 {
-    public string ElapsedTimer1 { get; private set; }
-    public string ElapsedTimer2 { get; private set; }
-    public bool AllQueriesComplete { get; private set; }
-    public bool Query1Complete { get; private set; }
-    public bool Query2Complete { get; private set; }
+    public string ElapsedTimer1
+    {
+        get
+        {
+            string elapsedString = "";
+            TimeSpan ts = stopwatch1.Elapsed;
+
+            // Format and display the TimeSpan value.
+            elapsedString = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            return elapsedString;
+        } 
+        private set {  } }
+    public string ElapsedTimer2
+    {
+        get
+        {
+            string elapsedString = "";
+            TimeSpan ts = stopwatch2.Elapsed;
+
+            // Format and display the TimeSpan value.
+            elapsedString = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            return elapsedString;
+        }
+        private set {  }
+    }
     Stopwatch stopwatch1 = new Stopwatch();
     Stopwatch stopwatch2 = new Stopwatch();
 
     public Queries()
     {
-        AllQueriesComplete = false;
-        Query1Complete = false;
-        Query2Complete = false;
         ElapsedTimer1 = "";
         ElapsedTimer2 = "";
     }
 
-    public bool QueriesComplete()
-    {
-        if (Query1Complete == true && Query2Complete == true)
-            return true;
-        else
-            return false;
-    }
-    async public void query1()
+    public void query1()
     {
         stopwatch1.Start();
-        await Task.Delay(TimeSpan.FromSeconds(5));
+        Thread.Sleep(5000);
         stopwatch1.Stop();
         stopwatch1.Reset();
-        Query1Complete = true;
     }
 
-    public string ElapsedTime1()
-    {
-        TimeSpan ts = stopwatch1.Elapsed;
-
-        // Format and display the TimeSpan value.
-        ElapsedTimer1 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-            ts.Hours, ts.Minutes, ts.Seconds,
-            ts.Milliseconds / 10);
-        return ElapsedTimer1;
-    }
-
-    public string ElapsedTime2()
-    {
-        TimeSpan ts = stopwatch2.Elapsed;
-
-        // Format and display the TimeSpan value.
-        ElapsedTimer2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-            ts.Hours, ts.Minutes, ts.Seconds,
-            ts.Milliseconds / 10);
-        return ElapsedTimer2;
-    }
-    async public void query2()
+    public void query2()
     {
         stopwatch2.Start();
-        await Task.Delay(TimeSpan.FromSeconds(10));
+        Thread.Sleep(10000);
         stopwatch2.Stop();
         stopwatch2.Reset();
-        Query2Complete = true;
     }
 }
 
